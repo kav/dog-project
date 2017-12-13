5a8c7cc0b5c2c85869a64fb86ea0e72a7d99a1a1
Kavanaugh Latiolais <kavlatiolais@gmail.com>
2017-12-13 16:32:53 -0500

    tweaking to better handle disconnects

--- setup.sh
+++ setup.sh
@@ -1,27 +1,13 @@
 #! /bin/bash
-if [ ! -f dogImages.zip ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
-  unzip dogImages.zip
-fi
-if [ ! -f lfw.zip ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
-  unzip lfw.zip
-fi
-
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
+unzip -f dogImages.zip
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
+unzip -f lfw.zip
 mkdir -p bottleneck_features
 cd bottleneck_features
-if [ ! -f DogVGG16Data.npz ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG16Data.npz
-fi
-if [ ! -f DogVGG19Data.npz ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG19Data.npz
-fi
-if [ ! -f DogResnet50Data.npz ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz
-fi
-if [ ! -f DogInceptionV3Data.npz ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogInceptionV3Data.npz
-fi
-if [ ! -f DogXceptionData.npz ]; then
-  wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogXceptionData.npz
-fi
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG16Data.npz
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG19Data.npz
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogInceptionV3Data.npz
+wget -c https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogXceptionData.npz
+
